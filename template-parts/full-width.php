b44ef65d59985afb7eb360cb0b0bce1c2affd689
nialldev <60255198+nialldev@users.noreply.github.com>
2023-05-02 03:55:36 +0100

    Add files via upload

---
+++ template-parts/full-width.php
@@ -0,0 +1,62 @@
+
+<?php
+
+/* 
+    Template Name: Full Width Template
+    Template Post Type: page
+    
+*/ ?>
+
+<?php 
+
+/**
+ * The main template file
+ */
+
+get_header();
+
+?>
+   <header class="header-home">   
+           <div class="header-vid" id='header-vid-img'>
+                  
+                   
+                </div>    
+            </header>
+    
+            <div class="main-container">
+              <div class="shows-block clearfix">
+               <div class="home-shows">
+                    <div class="title-wrapper"><h1><a href="shows.php">Shows</a></h1></div>
+                    <img src="img/Show.jpg">
+                    <div class="shows-home-block-text">
+                        <p>Cikada is a versatile show for all ages which can be performed both outdoors and in indoor venues.</p>
+                        
+                        <p>The piece is an acrobatic and poetic reflection of the balance between the organic and the human. With high level acrobatics and a graceful, pedestrian movement quality, the piece is a fun, thoughtful and humorous display of rich imagery and dynamic, riveting movement.</p>
+                        
+                        <p>Please go to the <a href="shows.php">show page</a> for more details.</p>
+                    </div>
+                </div><!-- end .home-shows -->
+                </div><!-- end .shows-block -->
+                <div class="about-block clearfix">
+              <div class="home-about">
+                  <div class="title-wrapper"><h1><a href="about.php">About</a></h1></div>
+                    <img src="img/SevE-Lightdrawing---Henrik_Ali---11-alt.jpg">
+                    <div class="block-text">
+                        <p>Ali (UK) &amp; Henrik (Sweden) are a hand to hand duo based out of Ireland. They began working together in May 2017 after both separately landing a job with Irish company Tumble Circus, and since then have been working as an acro-duo. They created their first show ‘Cikada’ at the beginning of 2018 which was nominated for the Judges’ Choice Award, ‘Best Duo,’ Dublin Fringe Festival 2018.</p>
+                    
+                        <p>Read more about Cikada Circus <a href="about.php">here</a>.</p>
+                    </div>
+                </div>
+                </div>
+                
+            <?php
+                get_footer();
+                
+            ?>
+      
+        </div>      
+          
+        
+        
+        
+    
